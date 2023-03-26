7df7009a6dca00b4b73ccfb614b6a929452460c8
zinnia <zinniazia.99@gmail.com>
2023-03-26 14:40:49 +0500

    mechanic pages done

--- src/sections/@travel/mechanic/expertprofile.js
+++ src/sections/@travel/mechanic/expertprofile.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import * as React from 'react';
 import userIcon from '@iconify/icons-carbon/user';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { TextIconLabel, Iconify, Image } from '../../../components';
 import { Icon } from '@iconify/react';
-import image from '../../../Assets/Images/expert2.jpg'
+import image from '../../../Assets/Images/expert2.jpg';
 import {
   Grid,
-  TableBody,
-  TableCell,
-  TableContainer,
+  List,
+  ListSubheader,
+  ListItem,
   TextField,
   Button,
   Stack,
@@ -17,7 +17,7 @@ import {
   Box,
   Typography,
 } from '@mui/material';
-import { Overview } from '../tours';
+import Comments from '../displaymaincar/comments';
 
 //--------------------------------------------------------------
 
@@ -26,7 +26,7 @@ Expertcall.propTypes = {
 };
 
 export default function Expertcall({ item }) {
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <Box
@@ -38,13 +38,34 @@ export default function Expertcall({ item }) {
       {/* {item.map((value) => ( */}
       {/* <Box sx={{ p: 3, boxShadow: '0 1px 10px #64666B', borderRadius: '8px',m:2 }}> */}
       <Grid container spacing={4} justifyContent="center">
-        <Grid item xs={12} sm={4} display="flex" alignItems="center">
-          <Image
-            src={image.src}
-            sx={{ width: '100%', height: 'auto', borderRadius: '10%' }}
-          />
+        <Grid item xs={12} sm={4} display="block" alignItems="center">
+          <Image src={image.src} sx={{ width: '100%', height: 'auto', borderRadius: '30px' }} />
+
+          <Typography variant="h3" mt={3}>Description</Typography>
+          <Box sx={{p:3,boxShadow: '0 1px 10px #64666B', borderRadius: '8px', m: 2 }}>
+            <Typography variant="body1">
+              Fusce quis eros at sem suscipit convallis eu ut odio. Aenean sagittis, mauris in
+              finibus mollis, dolor urna sollicitudin lacus, ac egestas dolor lectus ac nibh. In
+              placerat ante ligula, vitae mollis ante lobortis vel. Morbi et justo ut nisi mattis
+              ornare quis nec massa.{' '}
+            </Typography>
+          </Box>
+          <Typography variant="h3">Skills</Typography>
+          <Box sx={{ p: 4, boxShadow: '0 1px 10px #64666B', borderRadius: '8px', m: 2 }}>
+            <List
+              sx={{
+                listStyleType: 'disc',
+                '& .MuiListItem-root': {
+                  display: 'list-item',
+                },
+              }}
+            >
+              <ListItem>Double check your spelling</ListItem>
+              <ListItem>Your search </ListItem>
+            </List>
+          </Box>
         </Grid>
-        <Grid item xs={12} sm={8} mt={2}>
+        <Grid item xs={12} sm={8}>
           <Box sx={{ p: 3, boxShadow: '0 1px 10px #64666B', borderRadius: '8px', m: 2 }}>
             <Typography variant="h3">Asim Ihsan</Typography>
             <Typography variant="h4" mt={1}>
@@ -57,6 +78,7 @@ export default function Expertcall({ item }) {
               </Typography>
             </Stack>
           </Box>
+          <Comments/>
         </Grid>
       </Grid>
       {/* </Box> */}
