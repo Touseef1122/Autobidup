073db8de9036b7355da3237b99863d7f675e4614
Touseef1122 <105270240+Touseef1122@users.noreply.github.com>
2023-07-16 17:58:00 +0500

    New code

--- src/sections/@travel/auctionform/mainform.js
+++ src/sections/@travel/auctionform/mainform.js
@@ -32,14 +32,14 @@ export default function Mainform() {
     assemblyx: '',
   });
   const [formValues1p1, setFormValues1p1] = useState({
-    airbagsx: false,
-    acx: false,
-    alloy_wheelsx: false,
-    antibrakingsystemx: false,
-    cool_boxx: false,
-    folding_seatsx: false,
-    immoblizerx: false,
-    power_door_locksx: false,
+    airbagsx: 'False',
+    acx: 'False',
+    alloy_wheelsx: 'False',
+    antibrakingsystemx: 'False',
+    cool_boxx: 'False',
+    folding_seatsx: 'False',
+    immoblizerx: 'False',
+    power_door_locksx: 'False',
     // cupholdersx: false,
     // powersteeringx: false,
     // powerwindowsx: false,
@@ -54,8 +54,7 @@ export default function Mainform() {
     // armrestsx: false,
   });
   const [formValues2, setFormValues2] = useState({
-    images
-    : [],
+    images: [],
   });
   const [formValues3, setFormValues3] = useState({
     ad_titlex: '',
@@ -112,7 +111,7 @@ export default function Mainform() {
       }
       if (!formValues3.ad_descriptionx) {
         newErrors.ad_descriptionx = 'Discription is required';
-        isValid = false;
+        isValid = False;
       }
     }
     setErrors(newErrors);
@@ -123,8 +122,8 @@ export default function Mainform() {
     const isValid1 = validateForm(actStep);
     const isValid2 = validateForm(actStep);
     if (isValid1 && actStep == 0) {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    }else if (isValid2 && actStep == 1) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    } else if (isValid2 && actStep == 1) {
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
   };
@@ -138,15 +137,29 @@ export default function Mainform() {
       [name]: value,
     }));
   };
+
   const handleInputChange1p1 = (e) => {
     const value = e.target.checked;
     const name = e.target.name;
-
+    const capitalizedValue = value ? 'True' : 'False'
     setFormValues1p1((prevValues) => ({
       ...prevValues,
-      [name]: value,
+      [name]: capitalizedValue,
     }));
   };
+  // const handleInputChange1p1 = (e) => {
+  //   const value = e.target.checked;
+  //   const name = e.target.name;
+
+  //   setFormValues1p1((prevValues) => {
+  //     const updatedValues = { ...prevValues };
+  //     updatedValues[name] = value
+  //       ? value.toString().charAt(0).toUpperCase() + value.toString().slice(1)
+  //       : value;
+  //     return updatedValues;
+  //   });
+  // };
+
   const handleInputChange2 = (e) => {
     const files = e.target.files;
     const imageArray = Array.from(files);
@@ -199,34 +212,78 @@ export default function Mainform() {
       ...formValues1p1,
       ...formValues2,
       ...formValues3,
-      bids : item
+      bids: item,
       // created_at: formattedDate,
     });
-  }, [formValues1, formValues1p1, formValues2, formValues3]);
+  }, [formValues1, formValues1p1, formValues2,formValues3]);
 
+  // const handleSubmit = async () => {
+  //   console.log('Form Data', formData);
+  //   if (validateForm(2)){
+  //     console.log('submitteeedddd now');
+  //     console.log(formData);
+  //     try {
+  //       console.log('form is submiting');
+  //       const response = await fetch('https://autobidup.pythonanywhere.com/bidding/mainform', {
+  //         method: 'POST',
+  //         mode: 'cors',
+  //         credentials: 'include',
+  //         headers: {
+  //           'Content-Type': 'multipart/form-data',
+  //         },
+  //         body: JSON.stringify(formData),
+  //       });
+  //       if (response.ok) {
+  //         // API call successful
+  //         const responseData = await response.json();
+  //         console.log('response data', responseData);
+  //         console.log('form submitted succesfully');
+  //         setOpen(true);
+  //         //
+  //       } else {
+  //         // API call failed
+  //         const errorData = await response.json();
+  //         // Handle the error data as needed
+  //       }
+  //     } catch (error) {
+  //       // Error occurred during the API call
+  //       console.error(error);
+  //     }
+  //   }
+  // };
   const handleSubmit = async () => {
     console.log('Form Data', formData);
-    if (validateForm(2)){
+    if (validateForm(2)) {
       console.log('submitteeedddd now');
       console.log(formData);
       try {
-        console.log('form is submiting');
+        console.log('form is submitting');
+        const form = new FormData();
+
+        for (const key in formData) {
+          if (key === 'images') {
+            // Append each image file separately
+            for (let i = 0; i < formData.images.length; i++) {
+              form.append('images', formData.images[i]);
+            }
+          } else {
+            form.append(key, formData[key]);
+          }
+        }
+
         const response = await fetch('https://autobidup.pythonanywhere.com/bidding/mainform', {
           method: 'POST',
           mode: 'cors',
           credentials: 'include',
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          body: JSON.stringify(formData),
+          body: form,
         });
+
         if (response.ok) {
           // API call successful
           const responseData = await response.json();
           console.log('response data', responseData);
-          console.log('form submitted succesfully');
+          console.log('form submitted successfully');
           setOpen(true);
-          //
         } else {
           // API call failed
           const errorData = await response.json();
--- src/sections/@travel/displaymaincar/contactinfoAuction.js
+++ src/sections/@travel/displaymaincar/contactinfoAuction.js
@@ -15,14 +15,42 @@ import {
   Container,
   Box,
   Typography,
+  Button,
 } from '@mui/material';
 import { SellerinfoAuction, Sellerinfo } from './';
 
 //--------------------------------------------------------------
 
 export default function Contactinfo() {
+  const handleExitRoom = async () => {
+    try {
+      const response = await fetch(
+        'https://autobidup.pythonanywhere.com/bidding/exit_bidding_room',
+        {
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          // body: JSON.stringify({ bids: bid_Id }),
+        }
+      );
+
+      if (response.ok) {
+        // API call successful
+        console.log('Bidding room Exit successfully');
+        router.push('/')
+      } else {
+        // API call failed
+        console.error('Failed to Enter bidding room');
+      }
+    } catch (error) {
+      console.error('Error during API call:', error);
+    }
+  };
   return (
-    <Box >
+    <Box>
       <Box>
         <Typography variant="h3">Honda N Wgn G 2013</Typography>
         <Typography variant="h4" color="#CE9A00">
@@ -33,11 +61,19 @@ export default function Contactinfo() {
           <Icon icon={location} width="1.5vw" vAlign="middle" color="#CE9A00" />
           <Typography fontWeight="bold">Lahore</Typography>
         </Stack>
+        <Button
+          variant="contained"
+          size="large" // Set the size to "large"
+          onClick={handleExitRoom} // Call the handleExitRoom function on button click
+          target="_blank"
+          rel="noopener"
+        >
+          Exit
+        </Button>
       </Box>
       <Sellerinfo />
-      
-      <SellerinfoAuction />
 
+      <SellerinfoAuction />
     </Box>
   );
 }
