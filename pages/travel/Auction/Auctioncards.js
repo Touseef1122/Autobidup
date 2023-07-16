8d2299d956ffe33d2e5ef635556fe2cd0978dc02
Touseef1122 <105270240+Touseef1122@users.noreply.github.com>
2023-07-16 20:15:32 +0500

    bla

--- pages/travel/Auction/Auctioncards.js
+++ pages/travel/Auction/Auctioncards.js
@@ -96,129 +96,129 @@ export default function BlogMarketingLatestPosts({ bid_Id }) {
   }, []);
   console.log(data);
 
+  const carouselRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [alloted, setAlloted] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const carouselRef = useRef(null);
-    const [open, setOpen] = useState(false);
-    const [alloted, setAlloted] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const carouselSettings = {
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    rtl: Boolean(theme.direction === 'rtl'),
+    ...CarouselDots(),
+    responsive: [
+      {
+        breakpoint: theme.breakpoints.values.md,
+        settings: { slidesToShow: 3 },
+      },
+      {
+        breakpoint: theme.breakpoints.values.sm,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
 
-    const carouselSettings = {
-      dots: true,
-      arrows: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      rtl: Boolean(theme.direction === 'rtl'),
-      ...CarouselDots(),
-      responsive: [
+  const handleOpenRoom = async (bidId) => {
+    try {
+      const response = await fetch(
+        'https://autobidup.pythonanywhere.com/bidding/enter_bidding_room',
         {
-          breakpoint: theme.breakpoints.values.md,
-          settings: { slidesToShow: 3 },
-        },
-        {
-          breakpoint: theme.breakpoints.values.sm,
-          settings: { slidesToShow: 1 },
-        },
-      ],
-    };
-
-    const handleOpenRoom = async () => {
-      try {
-        const response = await fetch(
-          'https://autobidup.pythonanywhere.com/bidding/enter_bidding_room',
-          {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ bids: alloted }),
-          }
-        );
-
-        if (response.ok) {
-          // API call successful
-          let responseData = await response.json();
-          console.log(responseData)
-          s
-          console.log('Bidding room Enterence successfully');
-          router.push('/travel/Auction/BiddingDetails')
-        } else {
-          // API call failed
-          console.error('Failed to Enter bidding room');
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ bids: alloted }),
         }
-      } catch (error) {
-        console.error('Error during API call:', error);
+      );
+
+      if (response.ok) {
+        // API call successful
+        let responseData = await response.json();
+        console.log(responseData);
+        console.log('Bidding room Enterence successfully');
+        router.push({
+          pathname: '/travel/Auction/BiddingDetails',
+          query: { bid_Id: bidId },
+        });
+      } else {
+        // API call failed
+        console.error('Failed to Enter bidding room');
       }
-    };
-    const handlePrevious = () => {
-      carouselRef.current?.slickPrev();
-    };
+    } catch (error) {
+      console.error('Error during API call:', error);
+    }
+  };
+  const handlePrevious = () => {
+    carouselRef.current?.slickPrev();
+  };
 
-    const handleNext = () => {
-      carouselRef.current?.slickNext();
-    };
+  const handleNext = () => {
+    carouselRef.current?.slickNext();
+  };
 
-    return (
-      <RootStyle>
-        <Container>
-          <Typography
-            variant="h3"
-            paddingTop={'100px'}
+  return (
+    <RootStyle>
+      <Container>
+        <Typography
+          variant="h3"
+          paddingTop={'100px'}
+          sx={{
+            textAlign: 'center',
+          }}
+        >
+          Cars Available for Auction{' '}
+        </Typography>
+        <Box sx={{ position: 'relative' }}>
+          <CarouselArrows
+            onNext={handleNext}
+            onPrevious={handlePrevious}
             sx={{
-              textAlign: 'center',
+              '& .arrow': {
+                '&.left': { left: -20 },
+                '&.right': { right: -20 },
+              },
             }}
           >
-            Cars Available for Auction{' '}
-          </Typography>
-          <Box sx={{ position: 'relative' }}>
-            <CarouselArrows
-              onNext={handleNext}
-              onPrevious={handlePrevious}
-              sx={{
-                '& .arrow': {
-                  '&.left': { left: -20 },
-                  '&.right': { right: -20 },
-                },
-              }}
-            >
-              <Slider ref={carouselRef} {...carouselSettings}>
-                {data.map((value) => (
-                  <Box
-                    sx={{
-                      px: 2,
-                      py: { xs: 3, md: 4 },
-                    }}
-                  >
-                    <Box>
-                      <Box
-                        // onClick={handleOpen}
-                        onClick={handleOpenRoom}
-                        sx={{ p: 3, boxShadow: '0 1px 10px #64666B', borderRadius: '8px', mb: 1 }}
-                      >
-                        <ReverseCounter bid={value.bid_time} />
+            <Slider ref={carouselRef} {...carouselSettings}>
+              {data.map((value) => (
+                <Box
+                  sx={{
+                    px: 2,
+                    py: { xs: 3, md: 4 },
+                  }}
+                >
+                  <Box>
+                    <Box
+                      onClick={() => handleOpenRoom(value.bid_Id)} // Call the handleOpenRoom function with the bid_Id
+                      sx={{ p: 3, boxShadow: '0 1px 10px #64666B', borderRadius: '8px', mb: 1 }}
+                    >
+                      <ReverseCounter bid={value.bid_time} />
 
-                        <Image src={value.image} sx={{ width: '100%', height: '200px' }} />
-                        <Typography variant="h4">{`${value.make} ${value.model}`}</Typography>
-                        <Typography variant="h6">{value.year}</Typography>
-                        <Stack direction="row" justifyContent="space-between">
-                          <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
-                            {value.car_location}
-                          </Typography>
-                          <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
-                            {value.mileage}
-                          </Typography>
-                          <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
-                            {value.engine_type}
-                          </Typography>
-                        </Stack>
-                        <Typography variant="h4" color="#CE9A00">
-                          {' '}
-                          PKR {value.starting_bid}
+                      <Image src={value.image} sx={{ width: '100%', height: '200px' }} />
+                      <Typography variant="h4">{`${value.make} ${value.model}`}</Typography>
+                      <Typography variant="h6">{value.year}</Typography>
+                      <Stack direction="row" justifyContent="space-between">
+                        <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
+                          {value.car_location}
                         </Typography>
-                      </Box>
-                      {/* <Modal
+                        <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
+                          {value.mileage}
+                        </Typography>
+                        <Typography variant="body3" sx={{ display: { xs: 'none', sm: 'block' } }}>
+                          {value.engine_type}
+                        </Typography>
+                      </Stack>
+                      <Typography variant="h4" color="#CE9A00">
+                        {' '}
+                        PKR {value.starting_bid}
+                      </Typography>
+                    </Box>
+                    {/* <Modal
                       open={open}
                       onClose={handleClose}
                       aria-labelledby="modal-modal-title"
@@ -294,14 +294,13 @@ export default function BlogMarketingLatestPosts({ bid_Id }) {
                         </Button>
                       </Box>
                     </Modal> */}
-                    </Box>
                   </Box>
-                ))}
-              </Slider>
-            </CarouselArrows>
-          </Box>
-        </Container>
-      </RootStyle>
-    );
-  
+                </Box>
+              ))}
+            </Slider>
+          </CarouselArrows>
+        </Box>
+      </Container>
+    </RootStyle>
+  );
 }
--- pages/travel/Auction/BiddingDetails.js
+++ pages/travel/Auction/BiddingDetails.js
@@ -16,13 +16,13 @@ import Loader from '../UsedCars/Loader.js';
 // components
 import { Page, ErrorScreen, Breadcrumbs } from '../../../src/components';
 // sections
+import { useRouter } from 'next/router';
 import { styled } from '@mui/material/styles';
 import {
   CarouselAuction,
   Contactinfo,
   TravelTourDetailsAuction,
 } from '../../../src/sections/@travel/displaymaincar';
-// import {  } from '../../../src/sections/@travel';
 
 const RootStyle = styled('div')(({ theme }) => ({
   padding: theme.spacing(10, 0),
@@ -32,14 +32,13 @@ const RootStyle = styled('div')(({ theme }) => ({
   },
 }));
 
-// ----------------------------------------------------------------------
-
-// Registerchauffeur.propTypes = {
-//   posts: PropTypes.array.isRequired,
-// };
 
 export default function Displaycardetails({ posts }) {
   const { data: tours = [], error } = useRequest('/api/travel/tours');
+  
+  const router = useRouter();
+  const { bidId } = router.query;
+  const item = bidId ? JSON.parse(bidId) : null;
 
   if (error) {
     return <ErrorScreen />;
@@ -72,24 +71,16 @@ export default function Displaycardetails({ posts }) {
             <TravelTourDetailsAuction />
           </Grid>
           <Grid item xs={12} sm={5}>
-            {/* <Box><TravelTourDetails /></Box> */}
           </Grid>
         </Grid>
-        {/* tours={services} icons={summary} services={service}  */}
       </Container>
     </Page>
   );
 }
 
-//-------------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------
-
 Displaycardetails.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
-
-// ----------------------------------------------------------------------
 
 export async function getStaticProps() {
   return {
