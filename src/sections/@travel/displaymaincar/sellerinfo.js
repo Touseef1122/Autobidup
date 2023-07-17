960bb99bd8cccbd5edd7633ca5becdf0598a3d60
zinnia <zinniazia.99@gmail.com>
2023-07-17 10:20:30 +0500

    Bidding done

--- src/sections/@travel/displaymaincar/sellerinfo.js
+++ src/sections/@travel/displaymaincar/sellerinfo.js
@@ -42,18 +42,9 @@ export default function Contactinfo({ post, name, phone, car }) {
         };
         setComments([dict]);
       }
-      else if (car){
-        let dict = {
-          title: 'Seller Information',
-          name: name,
-          icon: userIcon,
-          text: phone_no,
-        };
-        setComments([dict]);
-      }
     }, []);
 
-    console.log(comments);
+    // console.log(comments);
    
     return (
       <Box sx={{ p: 3, boxShadow: '0 1px 10px #64666b', borderRadius: '8px', mt: 2 }}>
@@ -106,6 +97,77 @@ export default function Contactinfo({ post, name, phone, car }) {
         </Stack>
       </Box>
     );
+  }
+  if(car){
+   
+      const [comments, setComments] = useState([]);
+  
+      useEffect(() => {
+        if(car){
+          let dict = {
+            title: 'Seller Information',
+            name: car.bidding_car.name,
+            icon: userIcon,
+            text: car.bidding_car.phone_no,
+          };
+          setComments([dict]);
+        }
+      }, []);
+  
+      // console.log(comments);
+     
+      return (
+        <Box sx={{ p: 3, boxShadow: '0 1px 10px #64666b', borderRadius: '8px', mt: 2 }}>
+          {/* <Typography variant="h4" fontWeight="bold">Seller Information</Typography> */}
+          <Overview overview={comments} />
+  
+          {/* <Divider/> */}
+          <Stack
+            direction="row"
+            spacing={5}
+            mt={2}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            color="#CE9A00"
+          >
+            <Box
+              sx={{
+                p: 1,
+                boxShadow: '0 1px 10px #64666b',
+                borderRadius: '3px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Icon icon="material-symbols:add-call" width="30" />
+            </Box>
+            <Box
+              sx={{
+                p: 1,
+                boxShadow: '0 1px 10px #64666b',
+                borderRadius: '3px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Icon icon="mdi:envelope" width="30" />
+            </Box>
+            <Box
+              sx={{
+                p: 1,
+                boxShadow: '0 1px 10px #64666b',
+                borderRadius: '3px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Icon icon="ri:facebook-fill" width="30" />
+            </Box>
+          </Stack>
+        </Box>
+      );
+    
   }
 }
 
--- src/sections/@travel/displaymaincar/sellerinfoAuction.js
+++ src/sections/@travel/displaymaincar/sellerinfoAuction.js
@@ -1,216 +1,13 @@
-// import PropTypes from 'prop-types';
-// import * as React from 'react';
-// import userIcon from '@iconify/icons-carbon/user';
-// import { TextIconLabel, Iconify, Scrollbar } from '../../../components';
-// import { Icon } from '@iconify/react';
-// import { styled } from '@mui/material/styles';
-// import { useState } from 'react';
-
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TextField,
-//   Divider,
-//   Stack,
-//   Container,
-//   Box,
-//   ButtonGroup,
-//   Button,
-//   Typography,
-// } from '@mui/material';
-
-// import { OverviewAuction } from '../../../../src/sections/@travel/tours';
-
-// //--------------------------------------------------------------
-// const ScrollStyle = styled('div')(({ theme }) => ({
-//   [theme.breakpoints.up('sm')]: {
-//     maxWidth: '100%',
-//     padding: theme.spacing(1.5),
-//   },
-// }));
-// const comments = [
-//   {
-//     name: 'Asad Khan',
-//     text: '26 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Amir Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Adil Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Ayub Khan',
-//     text: '24 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Asad Khan',
-//     text: '26 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Amir Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Adil Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Ayub Khan',
-//     text: '24 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Asad Khan',
-//     text: '26 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Amir Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Adil Khan',
-//     text: '25 lack',
-//     icon: userIcon,
-//   },
-//   {
-//     name: 'Ayub Khan',
-//     text: '24 lack',
-//     icon: userIcon,
-//   },
-// ];
-
-// export default function Contactinfo({ post }) {
-//   console.log('post', post);
-//   const [counter, setCounter] = useState(500000);
-
-//   return (
-//     <Box>
-//       <Box
-//         sx={{
-//           p: 1,
-//           boxShadow: '0 1px 10px #64666b',
-//           borderRadius: '8px',
-//           mt: 2,
-//           // height: '150px',
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//         }}
-//       >
-//         <Box sx={{ width: '100%' }}>
-//           <ButtonGroup
-//             size="large"
-//             alingItem="center"
-//             variant="contained"
-//             aria-label="outlined primary button group"
-//           >
-//             {
-//               <Button
-//                 sx={{
-//                   backgroundColor: 'white',
-//                   border: '1px solid #FFBE00',
-//                   color: '#FFBE00',
-//                   '&:hover': { backgroundColor: '#FFBE00', color: 'white' },
-//                 }}
-//                 disabled={counter <= 0}
-//                 onClick={() => {
-//                   setCounter(counter - 10000);
-//                 }}
-//               >
-//                 -
-//               </Button>
-//             }
-//             {
-//               <Button disabled className="buttonCounter">
-//                 {counter}
-//               </Button>
-//             }
-//             <Button
-//               sx={{
-//                 backgroundColor: 'white',
-//                 border: '1px solid #FFBE00',
-//                 color: '#FFBE00',
-//                 '&:hover': { backgroundColor: '#FFBE00', color: 'white' },
-//               }}
-//               onClick={() => {
-//                 setCounter(counter + 10000);
-//               }}
-//             >
-//               +
-//             </Button>
-//           </ButtonGroup>
-//         </Box>
-//         <Box>
-//           <Button
-//             variant="contained"
-//             // onClick={() => router.push('/travel/buysellcar/form')}
-//             target="_blank"
-//             rel="noopener"
-//           >
-//             Bid
-//           </Button>
-//         </Box>
-//       </Box>
-//       <Box
-//         sx={{
-//           p: 3,
-//           boxShadow: '0 1px 10px #64666b',
-//           borderRadius: '8px',
-
-//           mt: 2,
-//           // height: '300px',
-//           // overflowY: 'scroll',
-//           height: '350px',
-//         }}
-//       >
-//         <Scrollbar>
-//           <ScrollStyle>
-//             <Typography variant="h4">Bidders List</Typography>
-//             <OverviewAuction overviewAuction={comments} />
-
-//             <Divider />
-//           </ScrollStyle>
-//         </Scrollbar>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// // ----------------------------------------------------------------------
-
-
-
 import PropTypes from 'prop-types';
 import * as React from 'react';
 import userIcon from '@iconify/icons-carbon/user';
 import { TextIconLabel, Iconify, Scrollbar } from '../../../components';
 import { Icon } from '@iconify/react';
 import { styled } from '@mui/material/styles';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TextField,
   Divider,
-  Stack,
-  Container,
   Box,
   ButtonGroup,
   Button,
@@ -226,22 +23,64 @@ const ScrollStyle = styled('div')(({ theme }) => ({
     padding: theme.spacing(1.5),
   },
 }));
-const comments = [
-  {
-    name: 'Asad Khan',
-    text: '26 lack',
-    icon: userIcon,
-  },
-  // Rest of the comments data
-];
+// const comments = [
+//   {
+//     name: 'Asad Khan',
+//     text: '26 lack',
+//     icon: userIcon,
+//   },
+// ]
 
 export default function Contactinfo({ post }) {
-  console.log('post', post);
-  const startingBid = parseInt(post?.bidding_car?.starting_bid || 0);
-  const [counter, setCounter] = useState(startingBid);
+  // console.log('post', post?.bidding_car?.starting_bid);
+  const [counter, setCounter] = useState('');
+  const [comments, setComments] = useState([]);
 
+  let startingBid = parseInt(post?.bidding_car?.starting_bid || 0);
+  useEffect(() => {
+    setCounter(startingBid)
+  }, []);
+  
   const handleBidClick = () => {
     console.log('Bid button clicked');
+    
+    async function fetchData() {
+      try {
+        console.log('details fetching');
+        const response = await fetch(
+          'https://autobidup.pythonanywhere.com/bidding/increase_bid',
+          {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        if (response.ok) {
+          // API call successful
+          let responseData = await response.json();
+          let dict = {
+            name: responseData.username,
+            icon: userIcon,
+            text: responseData['current bid'].toString(),
+          };
+          setComments((prevComments) => [...prevComments, dict]);
+        
+          console.log('response data', responseData);
+        } else {
+          // API call failed
+          const errorData = await response.json();
+          // Handle the error data as needed
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+
   };
 
   return (
@@ -288,9 +127,7 @@ export default function Contactinfo({ post }) {
                 color: '#FFBE00',
                 '&:hover': { backgroundColor: '#FFBE00', color: 'white' },
               }}
-              onClick={() => {
-                setCounter((prevCounter) => prevCounter + 10000);
-              }}
+              onClick={() => {setCounter((prevCounter) => prevCounter + 10000);}}
             >
               +
             </Button>
@@ -315,11 +152,7 @@ export default function Contactinfo({ post }) {
           <ScrollStyle>
             <Typography variant="h4">Bidders List</Typography>
             <OverviewAuction
-              overviewAuction={comments.map((comment) => ({
-                ...comment,
-                text: counter.toString(),
-                name: post?.name || 'User Name',
-              }))}
+              overviewAuction={comments}
             />
 
             <Divider />
